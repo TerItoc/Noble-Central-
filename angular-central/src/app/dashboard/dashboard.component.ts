44a33b2410605e0d64f87b5fa1bd5eb624bfbb87
Erick-Mex <69656531+Erick-Mex@users.noreply.github.com>
2022-05-06 14:49:10 -0500

    popover

--- angular-central/src/app/dashboard/dashboard.component.ts
+++ angular-central/src/app/dashboard/dashboard.component.ts
@@ -104,11 +104,11 @@ export class DashboardComponent implements OnInit {
 
   createTeams() {
     this.loading = true;
-    this.dsqls.getIfTeam().subscribe((res) => {
+    this.dsqls.getIfTeam().then((res) => {
       this.ifTeam = res;
 
       if (this.ifTeam) {
-        this.dsqls.getValidando().subscribe((res) => {
+        this.dsqls.getValidando().then((res) => {
           this.validando = res;
         });
 
