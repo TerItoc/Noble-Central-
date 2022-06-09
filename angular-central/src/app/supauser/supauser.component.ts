f049cf764c8df1cea5772e404acb02e8656440c7
Erick-Mex <69656531+Erick-Mex@users.noreply.github.com>
2022-06-09 16:01:01 -0500

    superadmin manejo de perfil

--- angular-central/src/app/supauser/supauser.component.ts
+++ angular-central/src/app/supauser/supauser.component.ts
@@ -2,45 +2,62 @@ import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { DashboardSqlService } from '../dashboard-sql.service';
 import { FormGroup, FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+const GRAPH_POINT = 'https://graph.microsoft.com/v1.0/me';
+
+type ProfileType = {
+  givenName?: string;
+  surname?: string;
+  userPrincipalName?: string;
+  id?: string;
+};
 
 @Component({
   selector: 'app-supauser',
   templateUrl: './supauser.component.html',
-  styleUrls: ['./supauser.component.css']
+  styleUrls: ['./supauser.component.css'],
 })
 export class SUPAUSERComponent implements OnInit {
-
+  profile!: ProfileType;
   constructor(
     private dsqls: DashboardSqlService,
-    private toastr: ToastrService
-  ) { }
+    private toastr: ToastrService,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    
+    this.http.get(GRAPH_POINT).subscribe((profile) => {
+      this.profile = profile;
 
+      this.dsqls.getIsAdmin(this.profile.userPrincipalName).subscribe((msg) => {
+        let value = Object.values(msg)[0];
+        if (value === 'No hay correo' || value === 'false') {
+          this.router.navigateByUrl('empleado');
+        }
+      });
+    });
   }
 
-  success : boolean = false;
+  success: boolean = false;
 
-  adminform= new FormGroup({
+  adminform = new FormGroup({
     nombre: new FormControl(''),
-    correo: new FormControl('')
+    correo: new FormControl(''),
   });
 
-  onSubmit(){
+  onSubmit() {
     this.dsqls.insertAdmin(this.adminform.value).subscribe((res) => {
       this.success = res['success'];
       console.log(res);
 
-      if(this.success){
-        this.toastr.success('', "Se inserto un administrador");
+      if (this.success) {
+        this.toastr.success('', 'Se inserto un administrador');
       } else {
-        this.toastr.error('', "Hubo un error al crear un administrador");
+        this.toastr.error('', 'Hubo un error al crear un administrador');
       }
     });
-
   }
-
-
-
 }
